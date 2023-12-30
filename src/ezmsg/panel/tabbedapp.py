a355c8e5354b6014936f0bb42f400fdc1fa1f028
Griffin Milsap <griffin.milsap@jhuapl.edu>
2023-12-30 09:31:17 -0500

    new pattern: tabbedapp

---
+++ src/ezmsg/panel/tabbedapp.py
@@ -0,0 +1,70 @@
+import abc
+import typing
+
+import panel as pn
+
+class Tab(abc.ABC):
+
+    @abc.abstractproperty
+    def name(self) -> str:
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    def sidebar(self) -> pn.viewable.Viewable:
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    def content(self) -> pn.viewable.Viewable:
+        raise NotImplementedError
+    
+
+class TabbedApp(abc.ABC):
+
+    @abc.abstractproperty
+    def title(self) -> str:
+        return ''
+
+    @abc.abstractproperty
+    def tabs(self) -> typing.List[Tab]:
+        return []
+    
+    @property
+    def tab_names(self) -> typing.List[str]:
+        return [t.name for t in self.tabs]
+
+    def app(self) -> pn.template.FastListTemplate:
+        
+        main = pn.Column(
+            sizing_mode = 'stretch_both'
+        )
+
+        sidebar = pn.Column(
+            sizing_mode = 'stretch_both'
+        )
+
+        tab_buttons = pn.widgets.RadioButtonGroup(
+            name = 'Tab Select',
+            options = self.tab_names,
+            button_style = 'outline',
+            sizing_mode = 'stretch_width',
+            orientation = 'vertical',
+        )
+
+        @pn.depends(tab_buttons, watch = True)
+        def tab_changed(tab: str) -> None:
+            tab_idx = self.tab_names.index(tab)
+            sidebar.clear()
+            sidebar.append(self.tabs[tab_idx].sidebar())
+            main.clear()
+            main.append(self.tabs[tab_idx].content())
+
+        if len(self.tab_names):
+            tab_changed(self.tab_names[0])
+
+        template = pn.template.FastListTemplate(
+            title = self.title, 
+            sidebar = [tab_buttons, pn.layout.Divider(), sidebar],
+            main = [main]
+        ) 
+
+        return template
